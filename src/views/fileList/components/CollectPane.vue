7f962745561cc8ea7c88df4d6215a19b38ce8042
林志斌 <linzb93@qq.com>
2024-10-23 13:26:35 +0800

    feat:完成文件上传历史功能

--- src/views/fileList/components/CollectPane.vue
+++ src/views/fileList/components/CollectPane.vue
@@ -1,116 +1,105 @@
 <template>
-  <el-dialog
-    title="收藏夹"
-    width="600px"
-    :model-value="visible"
-    @close="close"
-    @closed="closed"
-  >
-    <ul>
-      <li class="flexalign-center" v-for="item in collectList" :key="item.path">
-        <span class="dot"></span>
-        <span class="name" v-if="!item.isEdit">{{ item.name }}</span>
-        <el-input
-          size="small"
-          class="input-name"
-          v-model="item.name"
-          v-else
-          @keypress.native.stop.prevent.enter="item.isEdit = false"
-        />
-        <el-tooltip :content="item.path"
-          ><span class="path">{{ item.path }}</span></el-tooltip
-        >
-        <span class="icons">
-          <el-icon :size="18" @click="enter(item)" title="进入"
-            ><right
-          /></el-icon>
-          <el-icon :size="18" title="编辑" @click="item.isEdit = !item.isEdit"
-            ><edit
-          /></el-icon>
-          <el-icon :size="18" title="删除" @click="deleteItem(item)"
-            ><delete
-          /></el-icon>
-        </span>
-      </li>
-    </ul>
-    <el-button class="mt30" type="primary" @click="save">保存</el-button>
-  </el-dialog>
+    <el-dialog title="收藏夹" width="600px" :model-value="visible" @close="close" @closed="closed">
+        <ul v-if="collectList.length">
+            <li class="flexalign-center" v-for="item in collectList" :key="item.path">
+                <span class="dot"></span>
+                <span class="name" v-if="!item.isEdit">{{ item.name }}</span>
+                <el-input
+                    size="small"
+                    class="input-name"
+                    v-model="item.name"
+                    v-else
+                    @keypress.native.stop.prevent.enter="item.isEdit = false"
+                />
+                <el-tooltip :content="item.path"
+                    ><span class="path">{{ item.path }}</span></el-tooltip
+                >
+                <span class="icons">
+                    <el-icon :size="18" @click="enter(item)" title="进入"><right /></el-icon>
+                    <el-icon :size="18" title="编辑" @click="item.isEdit = !item.isEdit"><edit /></el-icon>
+                    <el-icon :size="18" title="删除" @click="deleteItem(item)"><delete /></el-icon>
+                </span>
+            </li>
+        </ul>
+        <el-empty v-else></el-empty>
+        <el-button class="mt30" type="primary" @click="save" v-if="collectList.length">保存</el-button>
+    </el-dialog>
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
-import request from "@/helpers/request";
-import { omit } from "lodash-es";
-import { ElMessage } from "element-plus";
-import { Edit, Delete, Right } from "@element-plus/icons-vue";
+import { ref, watch } from 'vue';
+import request from '@/helpers/request';
+import { omit } from 'lodash-es';
+import { ElMessage } from 'element-plus';
+import { Edit, Delete, Right } from '@element-plus/icons-vue';
 const props = defineProps({
-  visible: Boolean,
+    visible: Boolean,
 });
-const emit = defineEmits(["update:visible", "enter"]);
+const emit = defineEmits(['update:visible', 'enter']);
 
 const collectList = ref([]);
 
 watch(props, async ({ visible }) => {
-  if (!visible) {
-    return;
-  }
-  const list = await request("file-getCollect");
-  collectList.value = list.map((item) => ({
-    ...item,
-    isEdit: false,
-  }));
+    if (!visible) {
+        return;
+    }
+    const list = await request('file-getCollect');
+    collectList.value = list.map((item) => ({
+        ...item,
+        isEdit: false,
+    }));
 });
 
 const deleteItem = (target) => {
-  collectList.value = collectList.value.filter((item) => item.id !== target.id);
+    collectList.value = collectList.value.filter((item) => item.id !== target.id);
 };
 const enter = (target) => {
-  emit("enter", target.path);
-  close();
+    emit('enter', target.path);
+    close();
 };
 const save = async () => {
-  await request(
-    "file-saveCollect",
-    collectList.value.map((item) => omit(item, ["isEdit"]))
-  );
-  ElMessage.success("保存成功");
+    await request(
+        'file-saveCollect',
+        collectList.value.map((item) => omit(item, ['isEdit']))
+    );
+    ElMessage.success('保存成功');
 };
 const close = () => {
-  emit("update:visible", false);
+    emit('update:visible', false);
 };
 const closed = () => {
-  collectList.value = [];
+    collectList.value = [];
 };
 </script>
 <style lang="scss" scoped>
-@import "@/styles/mixin";
+@import '@/styles/mixin';
 li {
-  font-size: 16px;
+    font-size: 16px;
 }
 .input-name {
-  width: 120px;
-  margin-right: 10px;
+    width: 120px;
+    margin-right: 10px;
 }
 .dot {
-  width: 6px;
-  height: 6px;
-  background: #333;
-  border-radius: 50%;
-  margin-right: 10px;
+    width: 6px;
+    height: 6px;
+    background: #333;
+    border-radius: 50%;
+    margin-right: 10px;
 }
 .name {
-  @include textOverflow;
-  margin-right: 10px;
-  width: 120px;
+    @include textOverflow;
+    margin-right: 10px;
+    width: 120px;
 }
 .path {
-  @include textOverflow;
-  width: 200px;
+    @include textOverflow;
+    width: 200px;
 }
 .icons {
-  .el-icon {
-    margin-left: 10px;
-    cursor: pointer;
-  }
+    .el-icon {
+        margin-left: 10px;
+        cursor: pointer;
+    }
 }
 </style>
--- src/views/fileList/components/UploadHistory.vue
+++ src/views/fileList/components/UploadHistory.vue
@@ -1,80 +1,75 @@
 <template>
-  <el-drawer
-    :size="800"
-    :model-value="visible"
-    title="历史记录"
-    @close="close"
-    @closed="closed"
-  >
-    <el-table :data="list">
-      <el-table-column label="名称" prop="name">
-        <template #default="scope">
-          <el-link type="primary" @click="gotoFile(scope.row)">{{
-            pathUtil.basename(scope.row.path)
-          }}</el-link>
-        </template>
-      </el-table-column>
-      <el-table-column label="图片">
-        <template #default="scope">
-          <el-image class="preview" :src="scope.row.path" fit="cover" />
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-      background
-      class="mt30"
-      :total="totalCount"
-      hide-on-single-page
-      :page-size="query.pageSize"
-      v-model:current-page="query.pageIndex"
-      @change="getList"
-    />
-  </el-drawer>
+    <el-drawer :size="800" :model-value="visible" title="历史记录" @close="close" @closed="closed">
+        <el-table :data="list">
+            <el-table-column label="名称" prop="name">
+                <template #default="scope">
+                    <el-link type="primary" @click="gotoFile(scope.row)">{{
+                        pathUtil.basename(scope.row.path)
+                    }}</el-link>
+                </template>
+            </el-table-column>
+            <el-table-column label="图片">
+                <template #default="scope">
+                    <el-image class="preview" :src="`${props.domain}/${scope.row.path}`" fit="cover" />
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+            background
+            class="mt30"
+            :total="totalCount"
+            hide-on-single-page
+            :page-size="query.pageSize"
+            v-model:current-page="query.pageIndex"
+            @change="getList"
+        />
+    </el-drawer>
 </template>
 
 <script setup>
-import { ref, shallowRef, reactive, shallowReactive, watch } from "vue";
-import request from "@/helpers/request";
-import pathUtil from "@/helpers/path";
+import { ref, shallowRef, reactive, shallowReactive, watch } from 'vue';
+import request from '@/helpers/request';
+import pathUtil from '@/helpers/path';
 const props = defineProps({
-  visible: Boolean,
+    visible: Boolean,
+    domain: String,
 });
-const emit = defineEmits(["update:visible", "select"]);
+const emit = defineEmits(['update:visible', 'select']);
 watch(props, ({ visible }) => {
-  if (!visible) {
-    return;
-  }
-  getList();
+    if (!visible) {
+        return;
+    }
+    getList();
 });
 const query = shallowReactive({
-  pageSize: 10,
-  pageIndex: 1,
+    pageSize: 10,
+    pageIndex: 1,
 });
 const totalCount = shallowRef(0);
 
 const getList = async () => {
-  const result = await request("file-getHistory", query);
-  totalCount.value = result.totalCount;
-  list.value = result.list;
+    const result = await request('file-getHistory', query);
+    totalCount.value = result.totalCount;
+    list.value = result.list;
 };
 
 const list = shallowRef([]);
 const gotoFile = (row) => {
-  close();
-  emit("select", row.path);
+    close();
+    emit('select', row.path);
 };
 const close = () => {
-  emit("update:visible", false);
+    emit('update:visible', false);
 };
 const closed = () => {
-  query.pageIndex = 1;
-  list.value = [];
-  totalCount.value = 0;
+    query.pageIndex = 1;
+    list.value = [];
+    totalCount.value = 0;
 };
 </script>
 <style lang="scss" scoped>
 .preview {
-  width: 100px;
-  height: 100px;
+    width: 100px;
+    height: 100px;
 }
 </style>
